dbf2e2428324b4685e072b60b1e4939c4699b5a0
rossnewby <rossnewby@hotmail.co.uk>
2016-02-21 23:37:12 +0000

    Pause Menu Changes

--- src/controllers/PlayerMenu.java
+++ src/controllers/PlayerMenu.java
@@ -2,8 +2,11 @@ package controllers;
 
 import components.Animation;
 import components.Image;
+import components.Message;
+import components.Rect;
 import game.SpriteSheetLoad;
 import interfaces.Clickable;
+import org.jsfml.graphics.Color;
 import tools.Constants;
 
 import java.awt.image.BufferedImage;
@@ -13,12 +16,24 @@ import java.awt.image.BufferedImage;
  */
 public class PlayerMenu extends Menu {
     public final static String NAME = "Player Menu";
+    private int money = 100, lvl = 1;
 
     public PlayerMenu(){
         super(NAME);
 
-        //top bar
-        addSlot("LONG", 625, 150, 520, 70); //TODO resize / edit this with info
+        addSlot("LONG", 625, 150, 520, 70); //top panel
+
+        //add the players money
+        addEntity(new Message(430, 145, 0, Integer.toString(money), Color.WHITE, 20)); //TODO pass in actual currency
+        addEntity(new Image(500, 150, "assets" + Constants.SEP + "art" + Constants.SEP + "coins.png"));
+
+        addSlot("LVL", 585, 150, 85, 85); //level box
+        addEntity(new Message(585, 150, 0, Integer.toString(lvl), Color.WHITE, 30)); //TODO pass in actual level
+
+        //experiance bar
+        //TODO add functioning xp bar, this is just for show atm
+        addEntity(new Rect(null, 730, 150, 175, 15, Color.WHITE, 250));
+        addEntity(new Image(850, 150, "assets" + Constants.SEP + "art" + Constants.SEP + "xp_icon.png"));
 
         //left slots
         addSlot("HELMET", 500, 250);
