b2ae84e0cf871809674401abad1877459e388b9e
ruzzzzz <your@nan.com>
2016-02-23 22:12:44 +0000

    fixes cus im dumb

--- src/game/Room.java
+++ src/game/Room.java
@@ -343,7 +343,7 @@ public class Room extends RoomEntity implements MovementListener, ClickListener,
                 if(entity instanceof Enemy && ((Enemy)entity).Health < 0 && !((Enemy)entity).dead){
                     ((Enemy)entity).die();
                 }
-                if(entity instanceof Player && ((Player)entity).Health < 0){
+                if(entity instanceof Player && ((Player)entity).Health < 0 && !((Player)entity).dead){
                     ((Player)entity).die();
                     loadDrawer(GameOverMenu.class);
                     unload();
